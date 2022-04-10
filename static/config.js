75bd64ad0673051902f350903681be0bcf1c0466
李猛 <41979620+lfourg@users.noreply.github.com>
2022-04-10 13:45:12 +0800

    Update config.js

--- static/config.js
+++ static/config.js
@@ -1,7 +1,7 @@
 window.anovuiConfig = {
     serviceBasePath: "",
     uploadAction: "/management/management/v1/go-cases/v1/components/upload",
-    assetsBasePath: "/static/",
+    assetsBasePath: "./static/",
     uploadFileSize: {  //资源上传大小限制，单位M
         image: 5, 
         video: 20
